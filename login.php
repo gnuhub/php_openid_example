ac5dbca3177dc7d9415852b781e83567bc036be9
gnuhub <gnuhub@gmail.com>
2014-08-14 11:31:34 +0800

    add dump function

--- login.php
+++ login.php
@@ -1,5 +1,31 @@
 <?php
 require 'vendor/autoload.php';
+
+function dump($var, $echo=true, $label=null, $strict=true) {
+    $label = ($label === null) ? '' : rtrim($label) . ' ';
+    if (!$strict) {
+        if (ini_get('html_errors')) {
+            $output = print_r($var, true);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        } else {
+            $output = $label . print_r($var, true);
+        }
+    } else {
+        ob_start();
+        var_dump($var);
+        $output = ob_get_clean();
+        if (!extension_loaded('xdebug')) {
+            $output = preg_replace('/\]\=\>\n(\s+)/m', '] => ', $output);
+            $output = '<pre>' . $label . htmlspecialchars($output, ENT_QUOTES) . '</pre>';
+        }
+    }
+    if ($echo) {
+        echo($output);
+        return null;
+    }else
+        return $output;
+}
+
 try {
     # Change 'localhost:8080' to your domain name.
     $openid = new LightOpenID('localhost:8080');
@@ -22,14 +48,14 @@ try {
     input openid_identifier
     OpenID: <input type="text" name="openid_identifier" /> <button>Submit</button>
     -->
-    OpenID: <input type="hidden" name="openid_identifier" value="gnuhub.pip.verisignlabs.com" /> <button>Submit</button>
+    <input type="hidden" name="openid_identifier" value="gnuhub.pip.verisignlabs.com" /> <button>Login with OpenID</button>
 </form>
 <?php
     } elseif($openid->mode == 'cancel') {
         echo 'User has canceled authentication!';
     } else {
         echo 'User ' . ($openid->validate() ? $openid->identity . ' has ' : 'has not ') . 'logged in.';
-	print_r($openid->getAttributes());
+	dump($openid->getAttributes());
     }
 } catch(ErrorException $e) {
     echo $e->getMessage();
